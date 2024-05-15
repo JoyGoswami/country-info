c5f64c4d57b1d58780a0b20bd5cc3fc56f56b8ab
Joy Goswami <joy63586@gmail.com>
2024-05-15 15:23:25 +0600

    dark mode with local storage added

--- darkMode.js
+++ darkMode.js
@@ -4,5 +4,18 @@ const darkBtn = document.querySelector(".dark-btn");
 const body = document.querySelector("body");
 
 darkBtn.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
+  if (localStorage.getItem("mode") === null) {
+    localStorage.setItem("mode", "dark-mode");
+    body.classList.add("dark-mode");
+  } else if (localStorage.getItem("mode") === "dark-mode") {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("mode", "light");
+  } else if (localStorage.getItem("mode") === "light") {
+    body.classList.add("dark-mode");
+    localStorage.setItem("mode", "dark-mode");
+  }
 });
+body.classList.add(localStorage.getItem("mode"));
+console.log(localStorage.getItem("mode"));
+
+// body.classList.toggle(mode);
